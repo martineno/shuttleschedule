26f5422e5576d68d8ea77570da98fb6a46d43ed8
Martin Hecko <martin@fastmail.fm>
2013-06-22 12:16:20 -0700

    Initial implementation
    Still needs:
    
    * Caltrain shuttle
    * App manifest
    * Automatic "arriving" next updates

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,83 @@
 /*global define */
-define([], function () {
-    'use strict';
+define(['jquery'], function ($) {
+	'use strict';
 
-    return '\'Allo \'Allo!';
+	function ShuttleSchedule() {
+		this.timerId = null;
+	}
+
+	ShuttleSchedule.prototype.timeUpdater = function(timeDestination, frequency) {
+		if (this.timerId) {
+			clearTimeout(this.timerId);
+		}
+
+		this.timerId = setInterval(function() {
+			var now = new Date();
+			$(timeDestination).text(now.getHours() + ':' + now.getMinutes());
+		}, frequency);
+	};
+
+	/**
+	 * Given a reference to the table, returns the table data reference
+	 * that contains the next departi shuttle
+	 * @param  {STring} table The string representing the selector for the table
+	 * @return {Object}       The jQuery object that represents the data table data
+	 */
+	ShuttleSchedule.prototype.findNextShuttle = function(table) {
+		var toTownsend = this.getToTownsendArray(table),
+			toMarket = this.getToMarketArray(table),
+			now = new Date(),
+			minutesSinceMidnight = now.getHours() * 60 + now.getMinutes(),
+			nextArrival = function(array) {
+				var result = array.reduce(function(previousValue, item) {
+					if (previousValue === null && item.minutesSinceMidnight > minutesSinceMidnight) {
+						return item;
+					}
+
+					return previousValue;
+				}, null);
+
+				if (result === null) {
+					return $();
+				} else {
+					return result.$dom;
+				}
+			};
+
+		return nextArrival(toTownsend).add(nextArrival(toMarket));
+	};
+
+	ShuttleSchedule.prototype.getToTownsendArray = function(table) {
+		return this.getScheduleArray(table, 'tr td:first-child');
+	};
+
+	ShuttleSchedule.prototype.getToMarketArray = function(table) {
+		return this.getScheduleArray(table, 'tr td:nth-child(2)');
+	};
+
+	ShuttleSchedule.prototype.getScheduleArray = function(table, dataSelector) {
+		var $scheduleData = $(table + ' ' + dataSelector),
+			result = [];
+		$scheduleData.each(function() {
+			var $this = $(this),
+				hours = parseInt($this.data('hour'), 10),
+				minutes = parseInt($this.data('minute'), 10),
+				minutesSinceMidnight = hours * 60 + (minutes ? minutes : 0);
+
+			result.push({
+				minutesSinceMidnight: minutesSinceMidnight,
+				$dom: $this
+			});
+		});
+
+		return result;
+	};
+
+	ShuttleSchedule.prototype.applyLabelsOn = function($elements, label, style) {
+		var $label = $('<span>').text(label).addClass(style.join(' '));
+		$elements.each(function() {
+			$(this).append($label);
+		});
+	};
+	return ShuttleSchedule;
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,23 @@
 require.config({
-    paths: {
-        jquery: '../bower_components/jquery/jquery',
-        bootstrap: 'vendor/bootstrap'
-    },
-    shim: {
-        bootstrap: {
-            deps: ['jquery'],
-            exports: 'jquery'
-        }
-    }
+	paths: {
+		jquery: '../bower_components/jquery/jquery',
+		bootstrap: 'vendor/bootstrap'
+	},
+	shim: {
+		bootstrap: {
+			deps: ['jquery'],
+			exports: 'jquery'
+		}
+	}
 });
 
 require(['app', 'jquery', 'bootstrap'], function (app, $) {
-    'use strict';
-    // use app here
-    console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+	'use strict';
+	$(function() {
+		var ss = new app();
+		ss.timeUpdater('h1', 1000);
+
+		var shuttles = ss.findNextShuttle('table');
+		ss.applyLabelsOn(shuttles, 'Arriving next', ['label', 'label-info']);
+	});
 });
